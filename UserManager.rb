35bc6be39fdc7f52c991e74f3755ae9dd941a9c7
asachun <chuchuchu.chunchun@gmail.com>
2016-07-20 16:31:17 +0900

    Delete wasted array.

--- UserManager.rb
+++ UserManager.rb
@@ -2,14 +2,6 @@ require 'socket'
 require_relative './User'
 
 class UserManager
-	@@names = [
-		"ant", "bear", "cat", "dog", "eel",
-		"fox", "goat", "hippo", "jackal", "koala",
-		"lion", "mouse", "newt", "owl", "pig",
-		"quail", "rabbit", "sheep", "tiger", "unicon",
-		"viper", "wolf", "xyz", "yak", "zebla"
-	]
-
 	def initialize
 		@users = Array.new
 		@names = [
