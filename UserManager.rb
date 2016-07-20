a7a6b4c409fe4bc09fca355d59d11ef6644ee6b1
asachun <chuchuchu.chunchun@gmail.com>
2016-07-20 16:02:22 +0900

    Cure a bag.

--- UserManager.rb
+++ UserManager.rb
@@ -12,10 +12,18 @@ class UserManager
 
 	def initialize
 		@users = Array.new
+		@names = [
+			"ant", "bear", "cat", "dog", "eel",
+			"fox", "goat", "hippo", "jackal", "koala",
+			"lion", "mouse", "newt", "owl", "pig",
+			"quail", "rabbit", "sheep", "tiger", "unicon",
+			"viper", "wolf", "xyz", "yak", "zebla"
+		]
+		@names.shuffle!
 	end
 
 	def add_user socket
-		@users << User.new((@@names.sample 1), socket)
+		@users << User.new(@names.shift, socket)
 	end
 
 end
