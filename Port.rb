99fecaef3470585ff11cfd8e5ebf6dcf21e9b45e
asachun <chuchuchu.chunchun@gmail.com>
2016-07-21 21:44:53 +0900

    Divide Port Module.

---
+++ Port.rb
@@ -0,0 +1,21 @@
+module Port
+	def set_port_number argument, default_port
+		if argument.nil?
+			return default_port
+		end
+
+		begin
+			Integer argument
+		rescue ArgumentError
+			puts "Port number is invalid."
+			exit -1
+		end
+	end
+
+	def get_my_address
+		TCPSocket.gethostbyname(Socket::gethostname)[4]
+	end
+
+	module_function :set_port_number
+	module_function :get_my_address
+end
--- SharedVR.rb
+++ SharedVR.rb
@@ -1,31 +1,13 @@
 require 'socket'
+require_relative './Port'
 require_relative './User'
 require_relative './UserManager'
 
-def set_port_number (argument, default_port)
-	if argument.nil?
-		return default_port
-	end
-
-	begin
-		Integer argument
-	rescue ArgumentError
-		puts "Port number is invalid."
-		exit -1
-	end
-end
-
-def get_my_address
-	TCPSocket.gethostbyname(Socket::gethostname)[4]
-end
-
-
-
-port = set_port_number ARGV[0], 1435
+port = Port.set_port_number ARGV[0], 1435
 server = TCPServer.open port
 user_manager = UserManager.new
 
-puts "IP Address: #{get_my_address}."
+puts "IP Address: #{Port.get_my_address}."
 puts "Listen on #{port}."
 
 loop do
