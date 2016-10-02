7e6cfb5aa06a0bb9f0b983b776d446afcf158c0d
asachun <chuchuchu.chunchun@gmail.com>
2016-10-02 14:30:00 +0900

    Fix first Log message.

--- script/main.rb
+++ script/main.rb
@@ -7,8 +7,10 @@ port = Port.set_port_number ARGV[0], 1435
 server = TCPServer.open port
 user_manager = UserManager.new
 
+puts "\n-------------------------"
 puts "IP Address: #{Port.get_my_address}."
 puts "Listen on #{port}."
+puts "-------------------------"
 
 loop do
 	user_manager.add_user server.accept
