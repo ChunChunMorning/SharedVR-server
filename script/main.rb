a1f0fa1133ad45a7d0b6980e3b17e290b8111f78
asachun <chuchuchu.chunchun@gmail.com>
2017-11-27 10:11:25 +0900

    ログの順番を変更

--- script/main.rb
+++ script/main.rb
@@ -4,13 +4,14 @@ require_relative './user'
 require_relative './user_manager'
 
 port = Port.set_port_number ARGV[0], 1435
-server = TCPServer.open port
-user_manager = UserManager.new
 
 puts "\n-------------------------"
 puts "IP Address: #{Port.get_my_address}."
 puts "Listen on #{port}."
 puts "-------------------------"
+
+server = TCPServer.open port
+user_manager = UserManager.new
 
 loop do
 	user_manager.add_user server.accept
